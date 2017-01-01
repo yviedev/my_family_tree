1254f91cef0a7b9754d003706a1d126c37bfdec9
Yvonne Ike <yviedev@gmail.com>
2017-01-01 09:29:41 -0500

    Fix card width and height in users#index

--- app/models/user.rb
+++ app/models/user.rb
@@ -35,4 +35,12 @@ class User < ApplicationRecord
     end
   end
 
+  def month_date_year_time(date)
+    if date
+      date.strftime("%B %-d, %Y at %I:%M %P")
+    else
+      "#{first_name} doesn't have one."
+    end
+  end
+
 end
