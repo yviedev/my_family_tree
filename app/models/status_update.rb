2d0b13042a4c72350a5ef974c7e1a1b86366b7cd
Yvonne Ike <yviedev@gmail.com>
2017-01-08 12:08:26 -0500

    Edit form/input css

--- app/models/status_update.rb
+++ app/models/status_update.rb
@@ -9,4 +9,12 @@ class StatusUpdate < ApplicationRecord
     end
   end
 
+  def month_date_year_time(date)
+    if date
+      date.strftime("%B %-d, %Y at %I:%M %P")
+    else
+      "#{first_name} doesn't have one."
+    end
+  end
+
 end
