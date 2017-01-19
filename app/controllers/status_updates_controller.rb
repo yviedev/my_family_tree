2ddec16e9f3a9afc8d1625d784690ea6547cba55
Yvonne Ike <yviedev@gmail.com>
2017-01-18 20:22:04 -0500

    Add alerts to newsfeed for birthday and anniversary

--- app/controllers/status_updates_controller.rb
+++ app/controllers/status_updates_controller.rb
@@ -4,7 +4,17 @@ class StatusUpdatesController < ApplicationController
     @title = "My Newsfeed"
     if current_user
       @status_updates = Group.find(current_user.group_id).status_updates.order('created_at DESC').limit(5)
-      
+
+      @familymembers = User.where(group_id: current_user.group_id)
+      @familymembers.each do |familymember|
+        if familymember.birthday.strftime("%B %-d") == DateTime.now.strftime("%B %-d")
+          flash[:info] = "Hey! Today is #{familymember.first_name}'s birthday. Wish #{familymember.first_name} a happy birthday on your Newsfeed!"
+          p "Hey! Today is #{familymember.first_name}'s birthday. Wish them a happy birthday on your Newsfeed!"
+        elsif familymember.anniversary == DateTime.now
+          flash[:info] = "Hey! Today is #{familymember.first_name}'s anniversary. Wish #{familymember.first_name} a happy anniversary on your Newsfeed!"
+        end
+      end
+
       render 'index.html.erb'
     else
       redirect_to '/login'
