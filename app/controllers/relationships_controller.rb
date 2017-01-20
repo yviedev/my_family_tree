d4b3bc7c129e01bac3ef91eda1ac880b77860f9a
Yvonne Ike <yviedev@gmail.com>
2017-01-19 20:36:00 -0500

    Start inverse relationship logic

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -9,6 +9,7 @@ class RelationshipsController < ApplicationController
     @title = "Add relationship"
     @current_user = current_user
     @user = User.find(params[:user_id])
+    @relative_type_id = params[:relative_type_id]
 
     @relationship = Relationship.create!(
       user_id: current_user.id,
@@ -17,11 +18,20 @@ class RelationshipsController < ApplicationController
       group_id: current_user.group_id
     )
 
-    # if @relationship.save
-    #   @relationship1 = Relationship.create!(
-    #   user_id: current_user.id,
-    #   relative_id: params[:user_id],
-    #   relative_type_id: params[:relative_type_id]
+    # if @relationship && Relationship.where(user_id: params[:user_id]). where(relative_id: current_user.id).nil? && params[:user_id] != current_user.id
+    #   inverse_relationship = Relationship.create!(
+    #     user_id: params[:user_id],
+    #     relative_id: current_user.id,
+    #     relative_type_id: Relationship.relative_type_id_inverse
+    #   )
+    # elsif @relationship && Relationship.where(user_id: params[:user_id]). where(relative_id: current_user.id) && params[:user_id] != current_user.id
+    #   inverse_relationship = Relationship.where(user_id: params[:user_id]). where(relative_id: current_user.id)
+
+    #   inverse_relationship = Relationship.update(
+    #     id: inverse_relationship.id,
+    #     user_id: params[:user_id],
+    #     relative_id: current_user.id,
+    #     relative_type_id: Relationship.relative_type_id_inverse
     #   )
     # end
     
@@ -34,6 +44,7 @@ class RelationshipsController < ApplicationController
   end
 
   def edit
+    @title = "Edit Relationship"
     @relationship = Relationship.find(params[:id])
     @user = current_user
     @relative = User.find(@relationship.relative_id)
@@ -50,6 +61,25 @@ class RelationshipsController < ApplicationController
     @relationship.update(
       relative_type_id: params[:relative_type_id]
     )
+
+    # a = Relationship.where(user_id: params[:user_id]). where(relative_id: current_user.id). where(group_id: current_user.group_id)
+
+    # if @relationship && a.nil? && params[:user_id] != current_user.id
+    #   inverse_relationship = Relationship.create!(
+    #     user_id: params[:user_id],
+    #     relative_id: current_user.id,
+    #     relative_type_id: Relationship.relative_type_id_inverse
+    #   )
+    # elsif @relationship && a && params[:user_id] != current_user.id
+
+    #   inverse_relationship = Relationship.update(
+    #     id: a.first.id,
+    #     user_id: params[:user_id],
+    #     relative_id: current_user.id,
+    #     relative_type_id: Relationship.relative_type_id_inverse
+    #   )
+    # end
+   
 
     flash[:success] = "Congrats. You updated your family relationship."
     redirect_to "/familymembers/#{@user.id}"
--- app/models/relationship.rb
+++ app/models/relationship.rb
@@ -3,6 +3,46 @@ class Relationship < ApplicationRecord
   belongs_to :relative, class_name: User, optional: true
   belongs_to :relative_type, optional: true
 
-  def relative_info
+  def relative_type_id_inverse()
+    case relative_type_id
+      when 1
+        relative_type_id = 1
+      when 2
+        relative_type_id = 3
+      when 3
+        relative_type_id = 2
+      when 4
+        relative_type_id = 5
+      when 5
+        relative_type_id = 4
+      when 6
+        relative_type_id = 7
+      when 7
+        relative_type_id = 6
+      when 8
+        relative_type_id = 8
+      when 9
+        relative_type_id = 9
+      when 19
+        relative_type_id = 19
+    end
+    return relative_type_id
   end
+
+  def self.inverse_relationship (first_relative_type)
+    hash = Hash.new
+    hash[1] = 1
+    hash[2] = 3
+    hash[3] = 2
+    hash[4] = 5
+    hash[5] = 4
+    hash[6] = 7
+    hash[7] = 6
+    hash[8] = 8
+    hash[9] = 9
+    hash[19] = 19
+      
+    return hash[first_relative_type]
+  end
+
 end
