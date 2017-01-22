0ddbe85f0afe4b8dacf2203a4a67ac4a087a6b98
Yvonne Ike <yviedev@gmail.com>
2017-01-21 22:37:15 -0500

    Update family tree logic

--- app/controllers/trees_controller.rb
+++ app/controllers/trees_controller.rb
@@ -4,24 +4,32 @@ class TreesController < ApplicationController
 
     @trees = []
 
-    @trees_array = Relationship.where(user_id: current_user.id). where(group_id: current_user.group_id)
-    if @trees_array[0]
+    @trees_array = Relationship.where("user_id = ? OR relative_id = ?", current_user.id, current_user.id) 
+    if @trees_array.count > 0
       @trees_array.each do |relationship|
         if User.find(relationship.relative_id).images.first.nil?
           image = ""
         else
           image = User.find(relationship.relative_id).images.first.url
         end
+
+        if relationship.user_id == current_user.id
+          @relationship = RelativeType.find(relationship.relative_type_id).name
+        else
+          @relationship = RelativeType.find(relationship.relative_type_id).inverse_name
+        end 
+
         @trees << {
           user_id: relationship.relative_id,
           name: User.find(relationship.relative_id).first_name,
-          relationship: RelativeType.find(relationship.relative_type_id).name,
+          relationship: @relationship,
           image: image
         }
       end
     end
 
     # @trees.order('name ASC')
+    p @trees
 
     render 'donut.html.erb'
   end
