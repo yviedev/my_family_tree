b9953c2209b2229d447e4ac23ecc39cea3603106
Yvonne Ike <yviedev@gmail.com>
2017-01-29 18:23:25 -0500

    Edit migration file

--- db/migrate/20161211231332_edit_family_member_types1.rb
+++ db/migrate/20161211231332_edit_family_member_types1.rb
@@ -1,5 +1,5 @@
 class EditFamilyMemberTypes1 < ActiveRecord::Migration[5.0]
   def change
-    rename_table :family_member_types, :relative_types
+    # rename_table :family_member_types, :relative_types
   end
 end
